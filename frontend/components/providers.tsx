c8651391f0db4b036a17adfed6b83d60001f00ac
infinitethoughts7 <rakeshganji007@gmail.com>
2026-02-16 23:50:23 +0530

    Wire up QueryProvider, TooltipProvider, and Toaster in providers

--- frontend/components/providers.tsx
+++ frontend/components/providers.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { QueryProvider } from "@/providers/query-provider";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryProvider>
+        <TooltipProvider>
+          {children}
+          <Toaster richColors position="top-right" />
+        </TooltipProvider>
+      </QueryProvider>
+    </SessionProvider>
+  );
 }
